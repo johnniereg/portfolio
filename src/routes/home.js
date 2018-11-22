256179d4557fbf1ad4bfd0e71f05b9239574c8ba
johnniereg <johnniereg@gmail.com>
2018-11-21 20:55:30 -0800

    Style overhaul

--- src/routes/home.js
+++ src/routes/home.js
@@ -4,26 +4,24 @@ import { Link } from 'react-router-dom';
 class Home extends Component {
   render() {
     return (
-      <div className="Home">
-        <header className="Home-header">
-          <h1 className="Home-logo" style={{ position: `absolute`, top: 0 }}>
-            Johnnie Regalado
-          </h1>
-          <div className="Nav rotate">
-            <Link className="nav-item" to="/resume">
-              Resume
-            </Link>
-            <Link className="nav-item" to="/me">
-              About
-            </Link>
-            <Link className="nav-item" to="/tracks">
-              Tracks
-            </Link>
-          </div>
-          <p style={{ position: `absolute`, bottom: 0 }}>
-            a playground and portfolio
-          </p>
+      <div className="home">
+        <header>
+          <h1 className="nameplate">Johnnie Regalado</h1>
         </header>
+        <nav>
+          <Link className="nav-item" to="/resume">
+            Resume
+          </Link>
+          <Link className="nav-item" to="/me">
+            About
+          </Link>
+          <Link className="nav-item" to="/tracks">
+            Tracks
+          </Link>
+        </nav>
+        <footer>
+          <p>a playground and portfolio</p>
+        </footer>
       </div>
     );
   }
--- src/routes/tracks.js
+++ src/routes/tracks.js
@@ -9,11 +9,11 @@ class Tracks extends Component {
   }
 
   componentDidMount() {
-    let proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    // let proxyUrl = 'https://cors-anywhere.herokuapp.com/';
     let targetUrl =
       'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=johnniereg&api_key=907c0957fc6f58a0cdc5a95eacadb9bf&format=json&extended=1&limit=200http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=johnniereg&api_key=30ddf0e6d5fcc07ad9eb72f57214dbbd&format=json&extended=1&limit=200&from=1514764800';
 
-    fetch(proxyUrl + targetUrl)
+    fetch(targetUrl)
       .then(function(response) {
         return response.json();
       })
