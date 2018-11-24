d844164ac4ace20599c26aab0c158296c9040327
johnniereg <johnniereg@gmail.com>
2018-11-24 11:06:07 -0800

    Update route

--- src/routes/tracks.js
+++ src/routes/tracks.js
@@ -12,7 +12,7 @@ class Tracks extends Component {
     let proxyUrl = 'https://cors-anywhere.herokuapp.com/';
     let targetUrl =
       // 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=johnniereg&api_key=907c0957fc6f58a0cdc5a95eacadb9bf&format=json&extended=1&limit=200http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=johnniereg&api_key=30ddf0e6d5fcc07ad9eb72f57214dbbd&format=json&extended=1&limit=200&from=1514764800';
-      'https://morning-brushlands-94806.herokuapp.com/';
+      'https://morning-brushlands-94806.herokuapp.com/api/lastfm/recent-tracks';
 
     fetch(proxyUrl + targetUrl)
       .then(function(response) {
